3637feb47ce414d6274b0f235f9c54139e6faf4b
Evin1- <edwin.hdz.12.0@gmail.com>
2016-08-04 14:39:28 -0400

    Add Picasso logic

--- app/src/main/java/com/example/simpledoublefragment/MainActivity.java
+++ app/src/main/java/com/example/simpledoublefragment/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.simpledoublefragment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LeftFragment.CallbackComponent {
 
     private static final String LEFT_FRAGMENT_TAG = "LEFT_FRAGMENT";
     private static final String RIGHT_FRAGMENT_TAG = "RIGHT_FRAGMENT";
+
+    private static final String TAG = "MainActivityTAG_";
 
     private LeftFragment mLeftFragment;
     private RightFragment mRightFragment;
@@ -26,5 +29,13 @@ public class MainActivity extends AppCompatActivity {
                 .add(R.id.a_main_right, mRightFragment, RIGHT_FRAGMENT_TAG)
                 .commit();
 
+    }
+
+    @Override
+    public void buttonClicked(String name) {
+        Log.d(TAG, "buttonClicked: " + name);
+        if (mRightFragment != null && mRightFragment.isAdded()) {
+            mRightFragment.loadImage(name);
+        }
     }
 }
--- app/src/main/java/com/example/simpledoublefragment/RightFragment.java
+++ app/src/main/java/com/example/simpledoublefragment/RightFragment.java
@@ -2,10 +2,14 @@ package com.example.simpledoublefragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 
 /**
@@ -13,6 +17,7 @@ import android.view.ViewGroup;
  */
 public class RightFragment extends Fragment {
 
+    private ImageView mImageView;
 
     public RightFragment() {
         // Required empty public constructor
@@ -26,4 +31,17 @@ public class RightFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_right, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mImageView = (ImageView) view.findViewById(R.id.f_right_img);
+    }
+
+    public void loadImage(String name) {
+        String url = name.contains("Edwin")
+                ? "http://ichef-1.bbci.co.uk/news/660/cpsprodpb/025B/production/_85730600_monkey2.jpg"
+                : "http://kids.nationalgeographic.com/content/dam/kids/photos/animals/Mammals/H-P/pig-full-body.jpg.adapt.945.1.jpg";
+
+        Picasso.with(getContext()).load(url).into(mImageView);
+    }
 }
